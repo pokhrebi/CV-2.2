1004cc886e62eb6677de5fbac74b4b184a4cd385
Bhakti <61396982+pokhrebi@users.noreply.github.com>
2026-01-01 14:48:31 -0500

    test this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    // Make page visible after initialization
+    document.body.classList.add('page-loaded');
+  }
+}
